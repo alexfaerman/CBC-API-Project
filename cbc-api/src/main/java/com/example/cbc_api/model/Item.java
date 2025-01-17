6490abff36457aa2821a1af23095bb36bd15242a
Alex Faerman <133683036+alexfaerman@users.noreply.github.com>
2025-01-17 11:22:47 -0500

    Add files via upload

--- cbc-api/src/main/java/com/example/cbc_api/model/Item.java
+++ cbc-api/src/main/java/com/example/cbc_api/model/Item.java
@@ -1,1 +1,25 @@
+package com.example.cbc_api.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "items")
+public class Item {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String externalId;
+    private String title;
+    private String author;
+    private int publishedYear;
+    private String type;
+}
