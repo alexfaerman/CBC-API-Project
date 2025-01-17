4a7c3a3746a24c2c71f565d96fedabc0a5f767a3
Alex Faerman <133683036+alexfaerman@users.noreply.github.com>
2025-01-17 11:23:36 -0500

    Add files via upload

--- cbc-api/src/main/java/com/example/cbc_api/repository/ItemRepository.java
+++ cbc-api/src/main/java/com/example/cbc_api/repository/ItemRepository.java
@@ -1,1 +1,11 @@
+package com.example.cbc_api.repository;
+
+import com.example.cbc_api.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, UUID> {}
 
