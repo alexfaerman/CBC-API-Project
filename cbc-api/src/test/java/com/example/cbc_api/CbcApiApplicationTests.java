4202c1c02032c69b3463cea27ab69a6528019ba9
Alex Faerman <133683036+alexfaerman@users.noreply.github.com>
2025-01-17 11:28:46 -0500

    Add files via upload

--- cbc-api/src/test/java/com/example/cbc_api/CbcApiApplicationTests.java
+++ cbc-api/src/test/java/com/example/cbc_api/CbcApiApplicationTests.java
@@ -1,1 +1,13 @@
+package com.example.cbc_api;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
