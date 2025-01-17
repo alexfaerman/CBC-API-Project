119f1f2c96c23ecc9b39ed85fdaeffe6c07dc64a
Alex Faerman <133683036+alexfaerman@users.noreply.github.com>
2025-01-17 11:36:56 -0500

    Add files via upload

--- cbc-ui/services/apiService.js
+++ cbc-ui/services/apiService.js
@@ -1,1 +1,19 @@
+import axios from "axios";
 
+const BASE_URL = "http://localhost:8080/items";
+
+export const createItem = async (data, source) => {
+  return axios.post(`${BASE_URL}?ContentSource=${source}`, data);
+};
+
+export const bulkUpload = async (data) => {
+  return axios.post(`${BASE_URL}/bulk`, data);
+};
+
+export const getItemById = async (id) => {
+  return axios.get(`${BASE_URL}/${id}`);
+};
+
+export const getAllItems = async () => {
+  return axios.get(BASE_URL); 
+};
