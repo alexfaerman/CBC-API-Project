7f52dad14eb04aa687403d4833a452d36630c37c
Alex Faerman <133683036+alexfaerman@users.noreply.github.com>
2025-01-17 11:17:08 -0500

    Add files via upload

--- cbc-api/src/main/java/com/example/cbc_api/CbcApiApplication.java
+++ cbc-api/src/main/java/com/example/cbc_api/CbcApiApplication.java
@@ -1,1 +1,13 @@
+package com.example.cbc_api;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbcApiApplication.class, args);
+	}
+
+}
